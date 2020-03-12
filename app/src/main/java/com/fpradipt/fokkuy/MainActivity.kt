a506a82b7813539402d1ac685e117aa7309839f5
Dex <prdptlx@gmail.com>
2020-03-12 22:28:48 +0700

    first commit

---
+++ app/src/main/java/com/fpradipt/fokkuy/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fpradipt.fokkuy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
